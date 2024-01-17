0cc02be251124a779b90d05f76c0265a12cfc0c9
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-17 15:40:45 -0800

    Fix: Req & undefined ref

--- procFile.c
+++ procFile.c
@@ -7,23 +7,20 @@
  */
 void procFile(FILE *file, char *data, unsigned int line_number)
 {
-    size_t size = 0;
-    stack_t *stack = NULL;
-    ssize_t readdt = 1;
+	size_t size = 0;
+	stack_t *stack = NULL;
+	ssize_t readdt = 1;
 
-    while (readdt > 0)
-    {
-        data = NULL;
-        readdt = readLineFromFile(&data, &size, &line_number, file);
-        line_number++;
+	while (readdt > 0)
+	{
+	data = NULL;
+	readdt = readCmd(&data, &size, &line_number, file);
+	line_number++;
 
-        if (readdt > 0) {
-            execCmd(data, &stack, file, line_number);
-        }
+	if (readdt > 0)
+		execCmd(data, &stack, file, line_number);
+	}
 
-    }
-
-     free(data);
-
-    freeStack(&stack);
+	free(data);
+	freeStack(&stack);
 }
--- readData.c
+++ readData.c
@@ -7,22 +7,27 @@
  * @size: pointer to size of buffer
  * Return: number of chars read as ssize_t
  */
-ssize_t readLineFromFile(char **buffer, size_t *size, unsigned int *lnCount, FILE *file)
+ssize_t readCmd(char **buffer, size_t *size, unsigned int *lnCount, FILE *file)
 {
-    *buffer = NULL;
+	ssize_t readdt;
+	*buffer = NULL;
 
-    ssize_t readdt = getline(buffer, size, file);
+	readdt = getline(buffer, size, file);
 
-    if (readdt > 0) {
-        (*lnCount)++;
-        return readdt; 
-    } else if (readdt == -1) {
-        if (*buffer) {
-            free(*buffer);
-            *buffer = NULL;
-        }
-        onExit(NULL, EXIT_FAILURE); 
-    }
+	if (readdt > 0)
+	{
+	(*lnCount)++;
+	return (readdt);
+	}
+	else if (readdt == -1)
+	{
+		if (*buffer)
+		{
+		free(*buffer);
+		*buffer = NULL;
+		}
+	onExit(NULL, EXIT_FAILURE);
+	}
 
-    return 0; 
+	return (0);
 }
